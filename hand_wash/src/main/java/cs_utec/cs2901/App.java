d29c0cb842163c03cb9a50a51b1be8669d85e18f
ccaballerof00 <cristian.caballero@utec.edu.pe>
2020-04-01 17:23:55 -0500

    Trabajo terminado

---
+++ hand_wash/src/main/java/cs_utec/cs2901/App.java
@@ -0,0 +1,16 @@
+package cs_utec.cs2901;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+      for(int i = 0;i<100;i++)
+      {
+        System.out.println( "Debo lavarme las manos con agua y jabón por más de 20 segundos." );
+      }
+    }
+}
